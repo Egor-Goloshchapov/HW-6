2ad4fdd5fdf71ca0b01e5034a81e6be2c9affbc0
Egor-Goloshchapov <141495264+Egor-Goloshchapov@users.noreply.github.com>
2024-08-31 15:31:47 +0300

    Add files via upload

---
+++ hw6.js
@@ -0,0 +1,36 @@
+let counter = 1;
+while (counter < 11) {
+    console.log(counter)
+    counter +=1;
+}
+
+for (let i = 2; i < 22; i++) {
+    if (i % 2 == 0) {
+        console.log(i)
+    }
+}
+
+
+let numbers = 7;
+for (let i = 1; i <= 9; i++) {
+    console.log(`${numbers} * ${i} = ${numbers * i}`);
+}
+
+
+
+let number = 1;
+while (counter < 6) {
+    console.log(number)
+    counter +=1;
+}
+
+
+for (let i = 0; i < 11; i++) {
+    if (i === 7) {
+        continue;
+    } else {
+        console.log(i);
+    }
+}
+
+
